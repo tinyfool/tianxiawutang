744f8106896476c773b4a19e1582470858db3ae3
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2014-02-04 22:23:57 +0800

    limit user index page access control

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -29,14 +29,14 @@ class UserController extends Controller
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'users'=>array('admin'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'actions'=>array('create'),
+				'users'=>array('*'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('admin','delete','update'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
