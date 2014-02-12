34d84303597898312682d408e0bfbab601b687f6
Peiqiang Hao(Tinyfool) <tinyfool@gmail.com>
2014-02-12 09:10:52 +0800

    更改备案信息

--- themes/Bootstrap/views/layouts/tpl_footer.php
+++ themes/Bootstrap/views/layouts/tpl_footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <p>&copy; 天下无糖</p>
-  <p><a href="http://www.miitbeian.gov.cn/" target="_blank">沪ICP备14004046号</a>
+  <p><a href="http://www.miitbeian.gov.cn/" target="_blank">沪ICP备14004046号-1</a>
   </p>
 </footer>
 <script>
